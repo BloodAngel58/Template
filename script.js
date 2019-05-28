7f515ef6fd68f857ec66a3c4d9c7b29d9867ba1a
BloodAngel58 <bloodange2013@yandex.ru>
2019-05-28 09:53:01 +0300

    Added Date

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const todoList = ["Один", "Два", "Три", "Четыре"];
+const todoList = [
+  { NewTask: "Попить кофе", NewDate: "2019-05-03" },
+  { NewTask: "Погладить кота", NewDate: "2019-05-30" },
+  { NewTask: "Накормить кота", NewDate: "2019-05-01" },
+  { NewTask: "Уложить спать кота", NewDate: "2019-05-10" }
+];
 let listItem = document.getElementById("ListItem"); // div для вывод
 let buttonAdd = document.getElementById("add");
 
 function onclick() {
   let NewTask = document.getElementById("text").value;
-  todoList.push(NewTask);
+  let NewDate = document.getElementById("date").value;
+  let temp = {};
+  temp.NewTask = NewTask;
+  temp.NewDate = NewDate;
+  todoList.push(temp);
 
   let div = document.createElement("div");
   div.className = "div-out";
@@ -16,7 +25,7 @@ function OutTask() {
   for (let key = 0; key < todoList.length; key++) {
     let div = document.createElement("div");
     div.className = "div-out";
-    div.innerHTML = todoList[key];
+    div.innerHTML = todoList[key].NewTask + todoList[key].NewDate;
     listItem.appendChild(div);
   }
 }
