a9dcf60c57f6b45a8fcaa6f7724d7dafaefaebeb
BloodAngel58 <bloodange2013@yandex.ru>
2019-05-28 12:22:49 +0300

    Updated names,function to draw

--- script.js
+++ script.js
@@ -1,50 +1,49 @@
 const todoList = [
-  { NewTask: "Попить кофе", NewDate: "2019-05-03" },
-  { NewTask: "Погладить кота", NewDate: "2019-05-30" },
-  { NewTask: "Накормить кота", NewDate: "2019-05-01" },
-  { NewTask: "Уложить спать кота", NewDate: "2019-05-09" }
+  { title: "Попить кофе", date: "2019-05-03" },
+  { title: "Погладить кота", date: "2019-05-30" },
+  { title: "Накормить кота", date: "2019-05-01" },
+  { title: "Уложить спать кота", date: "2019-05-09" }
 ];
 let listItem = document.getElementById("ListItem"); // div для вывод
 let buttonAdd = document.getElementById("add");
 
 function onclick() {
-  let NewTask = document.getElementById("text").value;
-  let NewDate = document.getElementById("date").value;
-  let temp = {};
-  temp.NewTask = NewTask;
-  temp.NewDate = NewDate;
-  todoList.push(temp);
+  let title = document.getElementById("text").value;
+  let date = document.getElementById("date").value;
 
+  todoList.push({
+    title,
+    date
+  });
+  drawing(title, date);
+  Clear();
+}
+
+function Clear() {
+  document.getElementById("text").value = "";
+  document.getElementById("date").value = "";
+}
+
+function OutTask() {
+  for (let key = 0; key < todoList.length; key++) {
+    drawing(todoList[key].title, todoList[key].date);
+  }
+}
+
+function drawing(title, date) {
   let divText = document.createElement("div");
   divText.className = "single-todo__text";
   let divDate = document.createElement("div");
   divDate.className = "single-todo__date";
   let todoItem = document.createElement("div");
   todoItem.className = "single-todo";
-  divText.innerHTML = temp.NewTask;
-  divDate.innerHTML = temp.NewDate;
-
+  divText.innerHTML = title;
+  divDate.innerHTML = date;
   listItem.appendChild(todoItem);
   todoItem.appendChild(divText);
   todoItem.appendChild(divDate);
 }
 
-function OutTask() {
-  for (let key = 0; key < todoList.length; key++) {
-    let divText = document.createElement("div");
-    divText.className = "single-todo__text";
-    let divDate = document.createElement("div");
-    divDate.className = "single-todo__date";
-    let todoItem = document.createElement("div");
-    todoItem.className = "single-todo";
-    divText.innerHTML = todoList[key].NewTask;
-    divDate.innerHTML = todoList[key].NewDate;
-
-    listItem.appendChild(todoItem);
-    todoItem.appendChild(divText);
-    todoItem.appendChild(divDate);
-  }
-}
 buttonAdd.addEventListener("click", onclick);
 
 window.onload = function() {
